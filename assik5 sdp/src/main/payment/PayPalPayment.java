9d8bff58eb4d2e8d9ef86c82d2b6f76cd7d7f5af
Ulykbek1717 <dovutbekovulukbek@gmail.com>
2025-10-21 13:56:14 +0500

    commit

--- assik5 sdp/src/main/payment/PayPalPayment.java
+++ assik5 sdp/src/main/payment/PayPalPayment.java
@@ -1,7 +1,7 @@
 package main.payment;
 
 public class PayPalPayment implements Payment {
-    private final String email;
+    private  String email;
     private double balance;
 
     public PayPalPayment(String email, double balance) {
@@ -11,25 +11,26 @@ public class PayPalPayment implements Payment {
 
     @Override
     public void pay(double amount) {
-        if (balance >= amount) {
-            balance -= amount;
-            System.out.printf("Paid $%.2f via PayPal (%s).%n", amount, email);
-        } else {
-            System.out.println("Not enough PayPal balance!");
+        if (balance < amount) {
+            System.out.println("Not enough balance in PayPal: " + email);
+            return;
         }
+
+        balance -= amount;
+        System.out.println("Paid $" + amount + " via PayPal (" + email + ")");
     }
 
     @Override
     public void addCashback(double amount) {
         balance += amount;
-        System.out.printf("Cashback $%.2f added to PayPal (%s). New balance: $%.2f%n", amount, email, balance);
+        System.out.println("Cashback $" + amount + " added. New balance: $" + balance);
     }
 
     @Override
     public void addMoney(double amount) {
         balance += amount;
-        System.out.printf("Added $%.2f to PayPal (%s). Balance: $%.2f%n", amount, email, balance);
-    }
+        System.out.println("Added $" + amount + ". Balance: $" + balance);
+        }
 
     @Override
     public String toString() {
